12f584c81c39d5abf95df12dbfaeeb09624d470c
Benjamin Häublein <benjaminhaeublein@gmail.com>
2016-11-05 19:18:13 +0100

    executeScript makes changes in the original Password-Manager unnecessary

--- content.js
+++ content.js
@@ -1,5 +1,6 @@
 document.addEventListener('secretKeyReady', function(e){
     //send message to ext
+    console.log(e.detail);
     chrome.runtime.sendMessage({"request":"session", "data":e.detail}, function(response) {
         //callback
     });
@@ -11,3 +12,51 @@ document.addEventListener('loggedOut', function(e){
         //callback
     });
 }, false);
+
+function executeScript(script,args) {
+    var payload = '(' + script + ')('+JSON.stringify(args)+');';
+    var script = document.createElement('script');
+    script.textContent = payload;
+    (document.head||document.documentElement).appendChild(script);
+    script.remove();
+}
+
+console.log(103);
+chrome.runtime.sendMessage({"request":"actions"}, function(response) {
+    var request = JSON.parse(response);
+    switch(request["request"]){
+        case "login":
+            //data contains secretkey. It must be set using executeScript
+            break;
+        case "edit":
+            break;
+        case "addAccount":
+            break;
+    }
+});
+console.log(104);
+
+executeScript(function(){
+    var dataReadyOriginal = dataReady; 
+    dataReady = function(data) {
+        console.log("105 - before");
+        dataReadyOriginal(data);
+        console.log("100 - after");
+        var evt= new CustomEvent("secretKeyReady", {'detail':{'secretkey': secretkey, 'session_token': localStorage.session_token, 'confkey': sessionStorage.confusion_key, 'username':getcookie('username') }});
+        document.dispatchEvent(evt);
+    };
+    var quitpwdOriginal = quitpwd;
+    quitpwd = function() {
+        var evt= new CustomEvent("loggedOut", {});
+        document.dispatchEvent(evt);
+        quitpwdOriginal();
+    }
+    var quitpwd_untrustOriginal = quitpwd_untrust;
+    quitpwd_untrust = function() {
+        var evt= new CustomEvent("loggedOut", {});
+        document.dispatchEvent(evt);
+        quitpwd_untrustOriginal();
+    }
+    console.log(101);
+},null);
+console.log(102);
