16cc715fb427a773c9d8cd8723690efd77d4a1c9
Benjamin Häublein <benjaminhaeublein@gmail.com>
2016-12-03 09:32:08 +0100

    fixed regex

--- popup/options.js
+++ popup/options.js
@@ -1,17 +1,12 @@
 
 function ValidURL(str) {
-    var pattern = new RegExp('^(https?:\/\/)?'+ // protocol
-            '((([a-z\d]([a-z\d-]*[a-z\d])*)\.)+[a-z]{2,}|'+ // domain name
-                '((\d{1,3}\.){3}\d{1,3}))'+ // OR ip (v4) address
-            '(\:\d+)?(\/[-a-z\d%_.~+]*)*'+ // port and path
-            '(\?[;&a-z\d%_.~+=-]*)?'+ // query string
-            '(\#[-a-z\d_]*)?$','i'); // fragment locater
-    if(!pattern.test(str)) {
-        //alert("Please enter a valid URL.");
-        return false;
-    } else {
-        return true;
-    }
+  var pattern = new RegExp('^(https?:\\/\\/)?'+ // protocol
+  '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.?)+[a-z]{2,}|'+ // domain name
+  '((\\d{1,3}\\.){3}\\d{1,3}))'+ // OR ip (v4) address
+  '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // port and path
+  '(\\?[;&a-z\\d%_.~+=-]*)?'+ // query string
+  '(\\#[-a-z\\d_]*)?$','i'); // fragment locator
+  return pattern.test(str);
 }
 
 // Saves options to chrome.storage.sync.
