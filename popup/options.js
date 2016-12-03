3bd523888f9352942ccbcc750571aa30c56b6613
Benjamin Häublein <benjaminhaeublein@gmail.com>
2016-12-03 09:27:25 +0100

    check if url is valid

--- popup/options.js
+++ popup/options.js
@@ -1,33 +1,57 @@
 
+function ValidURL(str) {
+    var pattern = new RegExp('^(https?:\/\/)?'+ // protocol
+            '((([a-z\d]([a-z\d-]*[a-z\d])*)\.)+[a-z]{2,}|'+ // domain name
+                '((\d{1,3}\.){3}\d{1,3}))'+ // OR ip (v4) address
+            '(\:\d+)?(\/[-a-z\d%_.~+]*)*'+ // port and path
+            '(\?[;&a-z\d%_.~+=-]*)?'+ // query string
+            '(\#[-a-z\d_]*)?$','i'); // fragment locater
+    if(!pattern.test(str)) {
+        //alert("Please enter a valid URL.");
+        return false;
+    } else {
+        return true;
+    }
+}
+
 // Saves options to chrome.storage.sync.
 function save_options() {
-  var timeout = document.getElementById('timeout').value;
-  var url = document.getElementById('url').value;
-  chrome.storage.sync.set({
-    timeout: parseInt(timeout),
-    url: url
-  }, function() {
-    // Update status to let user know options were saved.
-    var status = document.getElementById('status');
-    status.textContent = 'Options saved.';
-    setTimeout(function() {
-      status.textContent = '';
-    }, 750);
-  });
+    var timeout = document.getElementById('timeout').value;
+    var url = document.getElementById('url').value;
+    if (!ValidURL(url)){
+        var status = document.getElementById('status').textContent= 'Please enter a valid URL';
+        return;
+    }
+    if (url.slice(-1)!="/"){
+        url += "/";
+        document.getElementById('url').value = url;
+    }
+
+    chrome.storage.sync.set({
+        timeout: parseInt(timeout),
+        url: url
+    }, function() {
+        // Update status to let user know options were saved.
+        var status = document.getElementById('status');
+        status.textContent = 'Options saved.';
+        setTimeout(function() {
+            status.textContent = '';
+        }, 750);
+    });
 }
 
 // Restores select box and checkbox state using the preferences
 // stored in chrome.storage.
 function restore_options() {
-  // Use default value color = 'red' and likesColor = true.
-  chrome.storage.sync.get({
-    timeout: 10,
-    url: ""
-  }, function(items) {
-    document.getElementById('timeout').value = items.timeout;
-    document.getElementById('url').value = items.url;
-  });
+    // Use default value color = 'red' and likesColor = true.
+    chrome.storage.sync.get({
+        timeout: 10,
+        url: ""
+    }, function(items) {
+        document.getElementById('timeout').value = items.timeout;
+        document.getElementById('url').value = items.url;
+    });
 }
 document.addEventListener('DOMContentLoaded', restore_options);
 document.getElementById('save').addEventListener('click',
-    save_options);
+        save_options);
